eae4eaa4aa79b732591ad8b7d7ab98da347df7cd
eric900115 <eric20607@gmail.com>
2023-07-06 17:40:28 +0800

    add Lab2

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -82,7 +82,15 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        IntList ptr = A;
+
+        while(ptr.rest != null){
+            ptr = ptr.rest;
+        }
+
+        ptr.rest = B;
+
+        return A;
     }
 
     /**
@@ -91,7 +99,18 @@ public class IntList {
      */
     public static IntList catenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        IntList res = new IntList(A.first, null);
+        IntList ptr = res;
+
+        while(A.rest != null){
+            A = A.rest;
+            ptr.rest = new IntList(A.first, null);
+            ptr = ptr.rest;
+        }
+
+        ptr.rest = B;
+
+        return res;
     }
 
 
